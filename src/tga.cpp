a048d7795b388a7ab82267a3e4475c863a5c278b
Caleb Solomon <cjsolo5@terpmail.umd.edu>
2026-06-22 20:19:38 -0400

    TGAHeader write() implementation]

--- src/tga.cpp
+++ src/tga.cpp
@@ -16,5 +16,24 @@ void TGAImage::set(int x, int y, TGAColor c) {
 }
 
 bool TGAImage::write(const std::string& path) const{
-    
+    // Open a file stream w/o interpreting certain bit sequences
+    std::ofstream out(path, std::ios::binary);
+    if (!out.is_open()) return false;
+
+    TGAHeader header;
+    header.image_type = 2; // uncompressed true color
+    header.img_width = w;
+    header.img_height = h;
+    header.pixel_depth = 32; // 4 bytes/pixel
+    // 0x20 for top-left pixel origin
+    // 0x8 signals 8 bits for alpha
+    header.img_descriptor = 0x20 | 8;
+
+    // Write the header to the file
+    out.write(reinterpret_cast<const char*>(&header), sizeof(TGAHeader));
+    // Write the content after. vector.data() returns a pointer to
+    // the first element of the vector
+    out.write(reinterpret_cast<const char*>(pixels.data()), w * h * sizeof(TGAColor));
+
+    return out.good();
 }
